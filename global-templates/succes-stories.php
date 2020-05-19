085d0ca47661064b85df16f2f7216784cc3392de
nikodimi <56118523+nikodimi@users.noreply.github.com>
2020-05-19 20:11:03 +0200

    lagt till success-stories på startsida
    Lagt till success-stories som en global-template och visat ut på frontpage efter senast ändrad cats

---
+++ global-templates/succes-stories.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Succes story setup
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+
+$succes_stories = new WP_Query([
+    'post_type'         =>      'cats',
+    'posts_per_page'    =>      3,
+    'meta_key'          =>      'cats_adopted',
+    'meta_value'        =>      'Yes',
+    'orderby'           =>      'modified',
+    'order'             =>      'DSC',
+    ]);
+
+    ?>
+
+    
+<div class="wrapper" id="wrapper-usps">
+
+    <div class="container">
+        <div class="row">
+        
+        <?php while($succes_stories->have_posts()) : $succes_stories->the_post(); ?>
+
+        <?php get_template_part('loop-templates/content', 'cats'); ?>
+
+<?php endwhile; ?>
+<?php wp_reset_postdata();
+?>
