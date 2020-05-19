7dc6d89e0d8b39e3023bf4646d2d10b36f7e9282
nikodimi <56118523+nikodimi@users.noreply.github.com>
2020-05-19 20:05:57 +0200

    Lagt till home
    Lagt till home och tillfällig vy för blogposts (aktuellt)

---
+++ loop-templates/content-newspost.php
@@ -0,0 +1,12 @@
+
+
+
+<div class="newspost-header">
+    <h1><?php the_title(); ?></h1>
+</div>
+<div class="img-container">
+    <img src="<?php the_post_thumbnail('medium'); ?>" alt="" class="newspost-thumbnail">
+</div>
+<div class="newspost-content">
+    <p><?php the_content(); ?></p>
+</div>
