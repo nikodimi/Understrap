04eb842eb079387d75dbe86014b6d6e3698bf208
nikodimi <56118523+nikodimi@users.noreply.github.com>
2020-05-22 22:22:00 +0200

    Lagt till knapp för länk i hero
    Lagt till knapp för länk i hero

--- global-templates/hero-front.php
+++ global-templates/hero-front.php
@@ -1,18 +1,27 @@
-<?php 
-/* 
-* Hero for the front page
-*/
+<?php
+/**
+ * Hero setup
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
 ?>
 
 <?php 
     $image = get_field('hero_image');
     $bg_color = get_field('hero_background_color');
+    $link = get_field('hero_link');
 ?>
 
 <section id="front-page-hero" style="background-color: <?php echo $bg_color; ?> ">
     <div class="container">
         <h1><?php the_field('hero_title'); ?></h1>
         <h1><?php the_field('hero_subtitle'); ?></h1>
+        <?php if(!empty($link)) : ?>
+            <a class="btn" href="<?php echo $link['url']; ?>">Link</a>
+        <?php endif; ?>
 
         <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
     </div>
