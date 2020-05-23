bc4c65c05a7f33d8ca9e9f132a417bcc3d06b09d
nikodimi <56118523+nikodimi@users.noreply.github.com>
2020-05-23 19:54:56 +0200

    Ändrat och stylat faqs
    Ändrat och stylat om mina faqs rutor

--- loop-templates/content-faqs.php
+++ loop-templates/content-faqs.php
@@ -1,8 +1,10 @@
-<div class="col-12 col-md-4 col-lg-4 m-auto">
-    <div class="card">
-        <div class="card-body text-center">
+<div class="col-12 col-md-6 col-lg-4 ">
+    <div class="faqs-box text-center">
+        <div class="title">
             <h3 class="card-title"><?php the_title(); ?></h3>
-            <p class="card-text"><?php the_content(); ?></p>
+        </div>
+        <div class="text">
+            <p><?php the_content(); ?></p>
         </div>
     </div>
 </div>
